51bbb6fc2d4946c6610c4740a989c2eb06d0f784
Samy <Rhalimsami8@gmail.com>
2024-12-17 01:50:53 +0100

    Fix the conflit

--- Micro-Services/Property-Service/src/main/java/ma/spacebnb/propertyservice/dao/repositories/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.spacebnb.propertyservice.dao.repositories;
-
-import ma.spacebnb.propertyservice.dao.entity.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
